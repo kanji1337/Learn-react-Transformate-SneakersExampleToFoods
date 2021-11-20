443417329154d05a65ead4c784077932cf953e4f
Sky <danil123.ddd@mail.ru>
2021-11-21 00:45:35 +0300

    purch

--- src/App.js
+++ src/App.js
@@ -3,15 +3,44 @@ function App() {
     <div className="wrapper clear">
       <div className="overlay">
         <div className="drawer">
-          <h2>Корзина</h2>
+          <div className="mb-20">
+            <h2>Корзина</h2>  
+          </div>
 
-          <div className="cartItem d-flex align-center">
+          <div className="items">
+          <div className="cartItem d-flex align-center mb-20">
             <img className="mr-20" src="/img/foods/1.png" width={104} height={104}/>
             <div className="mr-20">
               <p className="mb-5">Биг Мак Бекон</p>
               <b>149 руб</b>
             </div>
             <img class="remove" src="/img/btn_remove.svg" alt="remove" />
+          </div>
+          
+          <div className="cartItem d-flex align-center mb-20">
+            <img className="mr-20" src="/img/foods/1.png" width={104} height={104}/>
+            <div className="mr-20">
+              <p className="mb-5">Биг Мак Бекон</p>
+              <b>149 руб</b>
+            </div>
+            <img class="remove" src="/img/btn_remove.svg" alt="remove" />
+          </div>
+          </div>
+
+          <div className="cartTotalBlock">
+            <ul>
+                <li className="d-flex">
+                  <span>Итого:</span>
+                  <div></div>
+                  <b>298 руб.</b>
+                </li>
+                <li className="d-flex">
+                  <span>Доставка:</span>
+                  <div></div>
+                  <b>Бесплатно</b>
+                </li>
+            </ul>
+          <button>Оформить заказ</button>
           </div>
         </div>
       </div>
